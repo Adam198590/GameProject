184d000573f8e5bd9bc0ba72d49ee650bdd6a28e
Adam198590 <poch.adam.ta@gmail.com>
2016-02-28 21:36:38 +0100

    remove lib and utils

--- utils/protoGenerate/createProtoUnity.bat
+++
@@ -1,7 +0,0 @@
-﻿@echo Creating of proto class for C#/Unity
-
-start "" protoc.exe --proto_path=..\.. --descriptor_set_out=BinProto.protobin ..\..\repository\GameServer\res\protoconf\BasePacketsProto.proto
-
-start "" Google.ProtocolBuffers.2.4.1.555\tools\ProtoGen.exe BinProto.protobin
-
-::@pause
